001e9f8b8c31b8a78b8aec7c08d76ce659c61708
Paulo Viana <pcfviana@MacBook-Pro-de-Paulo.local>
2020-09-12 22:58:59 -0300

    Adicionando o controller de categorias

--- ProductCatalog/Startup.cs
+++ ProductCatalog/Startup.cs
@@ -1,12 +1,8 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using ProductCatalog.Data;
 
 namespace ProductCatalog
 {
@@ -16,7 +12,8 @@ namespace ProductCatalog
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc();
+            services.AddMvc(options => options.EnableEndpointRouting = false);
+            services.AddScoped<StoreDataContext, StoreDataContext>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -28,15 +25,6 @@ namespace ProductCatalog
             }
 
             app.UseMvc();
-            app.UseRouting();
-
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
-            });
         }
     }
 }
