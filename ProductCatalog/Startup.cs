548f79760fa4401af1581483490269e1376b009b
Paulo Viana <pcfviana@MacBook-Pro-de-Paulo.local>
2020-09-12 22:23:13 -0300

    Adição do MVC

--- ProductCatalog/Startup.cs
+++ ProductCatalog/Startup.cs
@@ -16,6 +16,7 @@ namespace ProductCatalog
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -26,6 +27,7 @@ namespace ProductCatalog
                 app.UseDeveloperExceptionPage();
             }
 
+            app.UseMvc();
             app.UseRouting();
 
             app.UseEndpoints(endpoints =>
